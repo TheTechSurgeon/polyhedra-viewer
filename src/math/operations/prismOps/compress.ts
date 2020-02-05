0d485c55727a4731b8c23409ddaa85e8174988c2
Nat Alison <tesseralis@gmail.com>
2020-02-04 22:43:35 -0800

    augmented cupolae

--- src/math/operations/prismOps/compress.ts
+++ src/math/operations/prismOps/compress.ts
@@ -61,35 +61,35 @@ function doCompress(polyhedron: Polyhedron) {
     polyhedron.name.includes(' cupola') &&
     polyhedron.name.includes('elongated')
   ) {
-    // const cap = Cap.getAll(polyhedron)[0];
-    // const base = cap.boundary();
-    // const compressSets = base.edges
-    //   .filter(e => e.face.numSides === 3)
-    //   .map(e => {
-    //     return {
-    //       face: e.twinFace(),
-    //       vertices: [...e.twinFace().vertices, e.next().v2],
-    //     };
-    //   });
-    // const resApothem =
-    //   base.sideLength() / (2 * Math.tan((2 * Math.PI) / base.numSides));
-    // const scale = base.apothem() - resApothem;
-    // const endVertices = getTransformedVertices(compressSets, ({ face }) => v =>
-    //   v.add(
-    //     face
-    //       .normal()
-    //       .getInverted()
-    //       .scale(scale),
-    //   ),
-    // );
+    const cap = Cap.getAll(polyhedron)[0];
+    const base = cap.boundary();
+    const compressSets = base.edges
+      .filter(e => e.face.numSides === 3)
+      .map(e => {
+        return {
+          face: e.twinFace(),
+          vertices: [...e.twinFace().vertices, e.next().v2],
+          polyhedron,
+        };
+      });
+    const resApothem =
+      base.sideLength() / (2 * Math.tan((2 * Math.PI) / base.numSides));
+    const scale = base.apothem() - resApothem;
+    const endVertices = getTransformedVertices(compressSets, ({ face }) => v =>
+      v.add(
+        face
+          .normal()
+          .getInverted()
+          .scale(scale),
+      ),
+    );
 
-    // return {
-    //   animationData: {
-    //     start: polyhedron,
-    //     endVertices,
-    //   },
-    // };
-    return polyhedron;
+    return {
+      animationData: {
+        start: polyhedron,
+        endVertices,
+      },
+    };
   }
   throw new Error('Polyhedron not supported yet');
 }
