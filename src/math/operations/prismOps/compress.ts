2dfda0357725b21d399128cbd1994c693f152b54
Nat Alison <tesseralis@gmail.com>
2020-02-05 20:50:36 -0800

    support all the prisms

--- src/math/operations/prismOps/compress.ts
+++ src/math/operations/prismOps/compress.ts
@@ -1,97 +1,133 @@
-import { Polyhedron, Cap } from 'math/polyhedra';
+import { Polyhedron, Cap, VEList, VertexList } from 'math/polyhedra';
 import makeOperation from '../makeOperation';
 import { getTransformedVertices } from '../operationUtils';
+import { inColumn } from '../../polyhedra/tableUtils';
+import { Vec3D } from '../../geom';
+
+interface CompressArgs<VL extends VertexList> {
+  /** The face-like regular polygon to reference when contracting */
+  base: VEList;
+  /** The list of `VertexList`s to apply compression to. */
+  compressSets: VL[];
+  /** The direction vector to push each Vertex set. Must be a unit vector. */
+  getDirVec: (set: VL) => Vec3D;
+}
+
+function compressVertices<VL extends VertexList>({
+  base,
+  compressSets,
+  getDirVec,
+}: CompressArgs<VL>) {
+  const resApothem =
+    base.sideLength() / (2 * Math.tan((2 * Math.PI) / base.numSides));
+  const scale = base.apothem() - resApothem;
+  const endVertices = getTransformedVertices(compressSets, set => {
+    return v => v.add(getDirVec(set).scale(scale));
+  });
+  return {
+    animationData: {
+      start: compressSets[0].polyhedron,
+      endVertices,
+    },
+  };
+}
 
 function doCompress(polyhedron: Polyhedron) {
-  if (polyhedron.name.includes(' prism')) {
-    // can always be guaranteed a base is the largest face b/c we only do compress
-    // on 6, 8, 10
+  if (inColumn(polyhedron.name, 'prisms', 'prism')) {
     const base = polyhedron.largestFace();
-    const compressFaces = base.adjacentFaces().filter((_, index) => index % 2);
-
-    const resApothem =
-      base.sideLength() / (2 * Math.tan((2 * Math.PI) / base.numSides));
-    const scale = base.apothem() - resApothem;
-
-    // move the vertices of these faces inward
-    const endVertices = getTransformedVertices(compressFaces, face => v =>
-      v.add(
-        face
-          .normal()
-          .getInverted()
-          .scale(scale),
-      ),
-    );
-
-    return {
-      animationData: {
-        start: polyhedron,
-        endVertices,
-      },
-    };
-  } else if (
-    polyhedron.name.includes(' cupola') &&
-    !polyhedron.name.includes('elongated')
-  ) {
-    const base = polyhedron.largestFace();
-    const compressFaces = base
-      .adjacentFaces()
-      .filter(face => face.numSides === 3);
-
-    const resApothem =
-      base.sideLength() / (2 * Math.tan((2 * Math.PI) / base.numSides));
-    const scale = base.apothem() - resApothem;
-    const endVertices = getTransformedVertices(compressFaces, face => {
-      const edge = face.edges.find(
-        e => e.twinFace().numSides === base.numSides,
-      )!;
-      const dir = base
-        .centroid()
-        .sub(edge.midpoint())
-        .getNormalized();
-      return v => v.add(dir.scale(scale));
+    return compressVertices({
+      base,
+      compressSets: base.adjacentFaces().filter((_, index) => index % 2),
+      getDirVec: face => face.normal().getInverted(),
     });
-    return {
-      animationData: {
-        start: polyhedron,
-        endVertices,
+  }
+  if (inColumn(polyhedron.name, 'capstones', '--')) {
+    const base = polyhedron.largestFace();
+    return compressVertices({
+      base,
+      // compressSets: base.adjacentFaces().filter(face => face.numSides === 3),
+      compressSets: base.edges
+        .filter(e => e.twinFace().numSides === 3)
+        .map(edge => ({
+          edge: edge.twin(),
+          vertices: edge.twinFace().vertices,
+          polyhedron,
+        })),
+      getDirVec: ({ edge }) => {
+        return base
+          .centroid()
+          .sub(edge.midpoint())
+          .getNormalized();
       },
-    };
-  } else if (
-    polyhedron.name.includes(' cupola') &&
-    polyhedron.name.includes('elongated')
-  ) {
+    });
+  }
+  if (inColumn(polyhedron.name, 'capstones', 'elongated')) {
     const cap = Cap.getAll(polyhedron)[0];
     const base = cap.boundary();
-    const compressSets = base.edges
-      .filter(e => e.face.numSides === 3)
-      .map(e => {
-        return {
-          face: e.twinFace(),
-          vertices: [...e.twinFace().vertices, e.next().v2],
-          polyhedron,
-        };
-      });
-    const resApothem =
-      base.sideLength() / (2 * Math.tan((2 * Math.PI) / base.numSides));
-    const scale = base.apothem() - resApothem;
-    const endVertices = getTransformedVertices(compressSets, ({ face }) => v =>
-      v.add(
-        face
-          .normal()
-          .getInverted()
-          .scale(scale),
-      ),
-    );
-
-    return {
-      animationData: {
-        start: polyhedron,
-        endVertices,
-      },
-    };
+    return compressVertices({
+      base,
+      compressSets: base.edges
+        .filter(e => e.face.numSides === 3)
+        .map(e => {
+          return {
+            face: e.twinFace(),
+            vertices: [...e.twinFace().vertices, e.next().v2],
+            polyhedron,
+          };
+        }),
+      getDirVec: ({ face }) => face.normal().getInverted(),
+    });
   }
-  throw new Error('Polyhedron not supported yet');
+  if (inColumn(polyhedron.name, 'capstones', 'bi-')) {
+    const cap = Cap.getAll(polyhedron)[0];
+    const base = cap.boundary();
+    return compressVertices({
+      base,
+      compressSets: base.edges
+        .filter(e => e.face.numSides === 3)
+        .map(e => {
+          return {
+            vertices: [...e.twinFace().vertices, e.next().v2],
+            polyhedron,
+            edge: e,
+          };
+        }),
+      getDirVec: ({ edge }) =>
+        base
+          .centroid()
+          .sub(edge.midpoint())
+          .getNormalized(),
+    });
+  }
+  if (inColumn(polyhedron.name, 'capstones', 'elongated bi-')) {
+    const cap = Cap.getAll(polyhedron)[0];
+    const base = cap.boundary();
+    return compressVertices({
+      base,
+      compressSets: base.edges
+        .filter(e => e.face.numSides === 3)
+        .map(edge => ({
+          edge,
+          polyhedron,
+          vertices: [
+            edge.next().v2,
+            ...edge.twinFace().vertices,
+            edge
+              .twin()
+              .next()
+              .next()
+              .twin()
+              .next().v2,
+          ],
+        })),
+      getDirVec: ({ edge }) =>
+        base
+          .centroid()
+          .sub(edge.midpoint())
+          .getNormalized(),
+    });
+  }
+  throw new Error('Unsupported polyhedron');
 }
 
 export const compress = makeOperation('compress', {
--- src/math/polyhedra/tableUtils.ts
+++ src/math/polyhedra/tableUtils.ts
@@ -10,31 +10,52 @@ import { toConwayNotation } from './names';
 // * Rows along a section like "cupola" or "pyramid"
 // * Multiple rows
 
-function hasDeep(collection: any, value: string): boolean {
+type Comparator = (s1: string, s2: string) => boolean;
+const defaultComparator = (s1: string, s2: string) => s1 === s2;
+
+function hasDeep(
+  collection: any,
+  value: string,
+  comparator: Comparator = defaultComparator,
+): boolean {
   if (collection instanceof Array) {
-    return _.some(collection, item => hasDeep(item, value));
+    return _.some(collection, item => hasDeep(item, value, comparator));
   } else if (collection instanceof Object) {
-    return _.some(_.values(collection), item => hasDeep(item, value));
+    return _.some(_.values(collection), item =>
+      hasDeep(item, value, comparator),
+    );
   } else {
-    return collection === value;
+    return comparator(collection, value);
   }
 }
+
+const dataComparator: Comparator = (s1, s2) => s1.replace('!', '') === s2;
 
 export function inRow(solid: string, sectionName: string, rowName: string) {
   const { rows, data } = sections[sectionName];
   const rowIndex = _.indexOf(rows, rowName);
   const row = data[rowIndex];
-  // TODO deal with '!' stuff in tables
-  return hasDeep(row, toConwayNotation(solid));
+  return hasDeep(row, toConwayNotation(solid), dataComparator);
 }
 
 export function inColumn(solid: string, sectionName: string, colName: string) {
   const { columns, data } = sections[sectionName];
-  const colIndex = _.indexOf(columns, colName);
-  return _.some(data, row => hasDeep(row[colIndex], toConwayNotation(solid)));
+  const colIndex = _.findIndex(columns, col => {
+    if (typeof col === 'string') {
+      return col === colName;
+    } else {
+      // TODO make more robust, maybe support array of strings?
+      return col.name === colName;
+    }
+  });
+  return _.some(data, row =>
+    hasDeep(row[colIndex], toConwayNotation(solid), dataComparator),
+  );
 }
 
 export function inSection(solid: string, sectionName: string) {
   const { data } = sections[sectionName];
-  return _.some(data, row => hasDeep(row, toConwayNotation(solid)));
+  return _.some(data, row =>
+    hasDeep(row, toConwayNotation(solid), dataComparator),
+  );
 }
